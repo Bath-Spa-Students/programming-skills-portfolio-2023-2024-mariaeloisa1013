dc7843c6d3d7bc0b2aa9a6aa9a058ef0c4c522d3
mariaeloisa1013 <mariaeloisa1013@gmail.com>
2024-03-02 10:32:45 +0400

    C2E2

---
+++ Chapter 2- Variables & Comments/exercise2.py
@@ -0,0 +1,9 @@
+#assign author to variable
+author = "Martin Luther King, Jr."
+
+#assign quote to variable
+quote = ''' "Out of the mountain of despair, a stone of hope." '''
+
+#print author with quote
+print (author + ' once said,' + quote)
+
