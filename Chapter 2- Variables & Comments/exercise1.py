1d05d801f35426fac38b74a758a154b2d277c412
mariaeloisa1013 <mariaeloisa1013@gmail.com>
2024-03-02 10:32:17 +0400

    C2E1

---
+++ Chapter 2- Variables & Comments/exercise1.py
@@ -0,0 +1,11 @@
+#assign a message to a variable
+var1 = "this is a sentence"
+
+#print variable
+print(var1)
+
+#change value of variable
+var1 = "this is a different sentence"
+
+#print new message
+print(var1)
