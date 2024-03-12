42882fc7b0aeaea053097bbdfd6a82f74998bda6
mariaeloisa1013 <mariaeloisa1013@gmail.com>
2024-03-12 18:06:41 +0400

    ch3e1

---
+++ Chapter 3- Data Structures/exercise1.py
@@ -0,0 +1,10 @@
+#store names of friends in a lisr
+names=['marwa', 'rinoa', 'kairi', 'phoebe', 'mj', 'alexa']
+
+#print each name per element 0-5
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
