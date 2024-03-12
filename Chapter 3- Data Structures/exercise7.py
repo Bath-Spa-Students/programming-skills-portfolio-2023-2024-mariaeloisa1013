af3ab0d54701564544ef112c6610faa21bbf2afd
mariaeloisa1013 <mariaeloisa1013@gmail.com>
2024-03-12 18:07:25 +0400

    ch3e7

---
+++ Chapter 3- Data Structures/exercise7.py
@@ -0,0 +1,37 @@
+#store five places in the world id visit in a list unalphabetically
+places = ["Japan", "USA", "Philippines", "Canada", "Paris"]
+
+#raw order
+print("\nOG order: ")
+print(places)
+
+#alphabetically
+print ("\nAlphabetically: ")
+print(sorted(places))
+
+#show normal list order
+print("\nOG order: ")
+print(places)
+
+#reverse
+print ("\nReverse: ")
+places.reverse()
+print(places)
+
+#reverse the reversed list
+print ("\nReverse again: ")
+places.reverse()
+print(places)
+
+#sort to alphabetical
+print ("\nAlphabetical again: ")
+places.sort()
+print(places)
+
+#reversed alphabetical
+print("\nReverse alphabet:")
+places.sort
+places.reverse()
+print(places)
+
+
