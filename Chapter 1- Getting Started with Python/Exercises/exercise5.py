a9d2bc4e4a29d915343a81dac56b6b93bd1b4322
mariaeloisa1013 <mariaeloisa1013@gmail.com>
2024-03-12 18:06:05 +0400

    ch1e5

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise5.py
@@ -0,0 +1,8 @@
+#allow float input into variable, radius
+radius = float(input("\brWhat's the radius of the circle? "))
+
+#assign varibale, area to solution
+area = (3.14*(radius**2))
+
+#print 
+print ("If the radius is " + str(radius) + ", then the area will be: " + str(area) + " cm \u00B2")
