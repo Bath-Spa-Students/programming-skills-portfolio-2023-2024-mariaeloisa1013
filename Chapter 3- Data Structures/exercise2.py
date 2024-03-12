1c01e67aa9cb3296ba2d4fb9ffebe5bf72f97ffb
mariaeloisa1013 <mariaeloisa1013@gmail.com>
2024-03-12 18:06:46 +0400

    ch3e2

---
+++ Chapter 3- Data Structures/exercise2.py
@@ -0,0 +1,10 @@
+#print the same list
+names=['marwa', 'rinoa', 'kairi', 'phoebe', 'mj', 'alexa']
+
+msg = "hello, goodmorning"
+print( msg , names[0])
+print( msg , names[1])
+print( msg , names[2])
+print( msg , names[3])
+print( msg , names[4])
+print( msg , names[5])
