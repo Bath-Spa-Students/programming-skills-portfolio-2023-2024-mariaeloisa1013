248bd23fa0fef8b07c34f4dce19fef7eb2ecaf17
mariaeloisa1013 <mariaeloisa1013@gmail.com>
2024-03-12 18:06:15 +0400

    ch2e3

---
+++ Chapter 2- Variables & Comments/exercise3.py
@@ -0,0 +1,15 @@
+#variable representing name
+me = " \tEloisa "
+
+#print normal version with whitespace
+print ("normal version:", "\n\t", me )
+
+#using lstrip
+print ("left strip:", "\n\t", me.lstrip())
+
+#using rstrip
+print ("right strip:", "\n\t", me.rstrip())
+
+#using strip
+print ("strip:", "\n\t", me.strip())
+
